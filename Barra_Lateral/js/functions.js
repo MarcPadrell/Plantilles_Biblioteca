48de579b0ba16370ef694898348d16ac9dec1f70
MarcPadrell <marc.padrell@hotmail.com>
2022-06-09 15:48:12 +0200

    Barra lateral feta. Register falta arreglar

---
+++ Barra_Lateral/js/functions.js
@@ -0,0 +1,8 @@
+let list = document.querySelectorAll('.navigation li')
+        function activeLink(){
+            list.forEach((item) =>
+            item.classList.remove(''));
+            this.classList.add('hovered');
+        }
+        list.forEach((item) =>
+        item.addEventListener('mouseover', activeLink))
